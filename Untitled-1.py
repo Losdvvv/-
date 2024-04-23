3e713e15e56225ddb767483b0468e11a414afbb9
Losdvvv <165806526+Losdvvv@users.noreply.github.com>
2024-04-23 18:57:33 +0500

    Add files via upload

---
+++ Untitled-1.py
@@ -0,0 +1,117 @@
+from pygame import *
+from random import randint
+from time import time as timer # импортируем функцию для засекания времени, чтобы интерпретатор не искал эту функцию в pygame модуле time, даем ей другое название сами
+# подгружаем отдельно функции для работы со шрифтом
+
+# Создаем окошко
+back = (200, 255, 255)
+win_width = 600
+win_height = 500
+window = display.set_mode((win_width, win_height))
+window.fill(back)
+
+
+
+font.init()
+font = font.Font(None, 35)
+lose1 = font.render('PLAYER 1 LOSE!', True, (180, 0, 0 ))
+lose2 = font.render('PLAYER 2 LOSE!', True, (180, 0, 0))
+
+game = True
+finish = False
+clock = time.Clock()
+FPS = 60
+
+
+speed_x = 3
+speed_y = 3
+
+# нам нужны такие картинки:
+img_racket = 'racket.png'
+ball = 'tenis_ball.png'
+
+
+
+# класс-родитель для других спрайтов
+class GameSprite(sprite.Sprite):
+ # конструктор класса
+   def __init__(self, player_image, player_x, player_y, player_speed, wight, height):
+       # Вызываем конструктор класса (Sprite):
+       sprite.Sprite.__init__(self)
+ 
+       # каждый спрайт должен хранить свойство image - изображение
+       #self.image = transform.scale(image.load(player_image), (size_x, size_y))
+       self.image = image.load(player_image)
+       self.speed = player_speed
+ 
+       # каждый спрайт должен хранить свойство rect - прямоугольник, в который он вписан
+       self.rect = self.image.get_rect()
+       self.rect.x = player_x
+       self.rect.y = player_y
+ # метод, отрисовывающий героя на окне
+   def reset(self):
+       window.blit(self.image, (self.rect.x, self.rect.y))
+ 
+# класс главного игрока
+class Player(GameSprite):
+   # метод для управления спрайтом стрелками клавиатуры
+   def update_r(self):
+        keys = key.get_pressed()
+        if keys[K_UP] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_DOWN] and self.rect.y < win_width - 80:
+            self.rect.y += self.speed
+
+
+   def update_l(self):
+        keys = key.get_pressed()
+        if keys[K_w] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_s] and self.rect.y < win_width - 80:
+            self.rect.y += self.speed
+
+#Ракетки
+racket1 = Player('racket.png', 30, 200, 4, 100, 150)
+racket2 = Player('racket.png', 520, 200, 4, 100, 150)
+ball = GameSprite('tenis_ball.png', 60, 200, 5, 50, 30)
+
+while game:
+    for e in event.get():
+        if e.type == QUIT:
+            game = False
+
+    if finish != True:
+        window.fill(back)
+        racket1.update_l()
+        racket2.update_r()
+        ball.rect.x += speed_x
+        ball.rect.y += speed_y
+
+        if sprite.collide_rect(racket1, ball) or sprite.collide_rect(racket2, ball):
+            speed_x *= -1
+            speed_y *= 1
+
+        if ball.rect.y > win_height-50 or ball.rect.y < 0:
+            speed_y *= -1
+
+        if ball.rect.x < 0:
+            finish = True
+            window.blit(lose1, (200, 200))
+
+        if ball.rect.x > win_width:
+            finish = True
+            window.blit(lose2, (200, 200))
+            game_over = True
+
+
+        racket1.reset()
+        racket2.reset()
+        ball.reset()
+            
+
+    display.update()
+    clock.tick(FPS)
+
+
+
+
